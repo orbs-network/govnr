ec9f64f3e65d2be6c7f21a2ac93a97b40b8acb52
Shai Yallin <shai.yallin@gmail.com>
2019-08-12 17:45:23 +0300

    fixed doc

--- doc.go
+++ doc.go
@@ -2,10 +2,10 @@
 Package govnr provides the facilities for running supervised, persistent goroutines ("background services") that, upon unexpected panics, re-run the provided function.
 This is similar to other libraries such as https://github.com/thejerf/suture, with several differences:
 
-1. govnr accepts a context.Context and listens to its .Done() channel
+  1. govnr accepts a context.Context and listens to its .Done() channel
 
-2. starting a persistent process requires nothing more than calling govnr.Forever()
+  2. starting a persistent process requires nothing more than calling govnr.Forever()
 
-3. no reliance on global state
+  3. no reliance on global state
  */
 package govnr
